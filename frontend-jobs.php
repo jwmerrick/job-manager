eef19c351a6f5fe3ee10834ad7bdc8a48753268d
Jason Merrick <jmerrick@talosengineering.com>
2025-12-04 12:21:24 -0800

    Removed setting page_id to -1 for category listing.  No idea why that was there.

--- frontend-jobs.php
+++ frontend-jobs.php
@@ -8,7 +8,6 @@ function jobman_display_jobs_list( $cat ) {
 	$page = get_post( $options['main_page'] );
 
 	if( 'all' != $cat ) {
-		$page->ID = -1;
 		$page->post_type = 'jobman_joblist';
 		$page->post_title = __( 'Jobs Listing', 'jobman' );
 	}
